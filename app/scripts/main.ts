a0ff868b789e4287f5efb95289555360b131f3f7
fcamblor <fcamblor@gmail.com>
2017-05-18 00:25:08 +0200

    Added service worker putting pictures into cache to avoid downloading it several times

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -3,6 +3,12 @@ import {PicturesLoader} from './components/PicturesLoader';
 import {PicturesGallery} from './components/PicturesGallery';
 
 
+// register service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw-cache-images.js', { scope: './' })
+        .then((reg) => console.log('SW Registration succeeded. Scope is ' + reg.scope))
+        .catch((error, ...args) => console.error('Error when registering service worker', error, args));
+}
 
 let picturesLoader = new PicturesLoader();
 picturesLoader.load().then(() => {
