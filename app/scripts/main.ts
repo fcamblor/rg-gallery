b2320bb358ec73488d418842adf51b2ad7c41749
fcamblor <fcamblor@gmail.com>
2017-05-20 10:04:09 +0200

    Allowing to force unregistration of service workers when providing forceResetSW query param

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -2,17 +2,30 @@ import {PicturesLoader} from './components/PicturesLoader';
 import {PicturesGallery} from './components/PicturesGallery';
 import {DrawingItem} from './components/DrawingItem';
 
+let getParameterByName = (name: string) => {
+    let url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 
 // register service worker
 if ('serviceWorker' in navigator) {
-    // Unregistering old sw-cache-images service worker
+    // Unregistering old sw-cache-images service worker and/or ALL service workers if forceResetSW query param is provided
+    let forceResetSW = getParameterByName("forceResetSW");
     navigator.serviceWorker.getRegistrations().then(function(registrations) {
         _.each(registrations, (registration) => {
-            if(_.get(registration, 'active.scriptURL', '').indexOf('sw-cache-images') !== -1) {
+            let swUrl = _.get(registration, 'active.scriptURL', '');
+            if(swUrl.indexOf('sw-cache-images') !== -1 || forceResetSW) {
                 registration.unregister().then(function(register){
-                    console.info("Old service worker unregistered successfully : "+register);
+                    console.info("Old service worker unregistered successfully : "+swUrl);
+                    alert("Old service worker unregistered successfully : "+swUrl);
                 }, function(error){
-                    console.error("Old service worker unregistration failed : "+error);
+                    console.error("Old service worker unregistration failed "+swUrl+" : "+error);
+                    alert("Error during old service worker unregistration "+swUrl+" : "+error);
                 });
             }
         });
