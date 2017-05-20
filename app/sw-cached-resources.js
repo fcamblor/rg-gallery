09d669e057811051bc696a7391a6b13ff6f0dd63
fcamblor <fcamblor@gmail.com>
2017-05-20 10:03:35 +0200

    Improved service worker cache management, still returning response when we're not able to put asset in cache (for quota exceeded reasons for instance)

--- app/sw-cached-resources.js
+++ app/sw-cached-resources.js
@@ -4,6 +4,31 @@ var isAsset = function(url){ return url[url.length-1]==='/' || url.indexOf('inde
 var isPicture = function(url){ return url.indexOf('r-gounot.fr') !== -1 && !isAsset(url); };
 var isSpreadsheet = function(url) { return url.indexOf('spreadsheets.google.com') !== -1; };
 
+var findResponseInCache = function(cache, request) {
+    return cache.match(request).then(function (response) {
+        if(response) {
+            console.info("Used cached asset for : "+request.url);
+        } else {
+            console.error("Asset neither available from network nor from cache : "+request.url);
+        }
+        return response;
+    }, function(error) {
+        console.error("Error while matching request "+request.url+"in cache : "+error);
+    });
+};
+
+var storeResponseInCache = function(what, cache, request, response) {
+    return cache.put(request, response.clone()).then(function(){
+        console.info("Stored "+what+" into cache : " + request.url);
+        return response;
+    }, function(error){
+        console.error("Error while storing response for "+request.url+" : "+error);
+        // Still returning initial response : if we face quota exceeded error, it shouldn't prevent
+        // the asset to be fetched/returned
+        return response;
+    });
+};
+
 this.addEventListener('fetch', function fetcher (event) {
     var request = event.request;
     // check if request
@@ -12,25 +37,16 @@ this.addEventListener('fetch', function fetcher (event) {
         // Asset detected : serving from network first, then falling back on cache if not available
         event.respondWith(
             caches.open('rg-assets').then(function(cache) {
-                var fetchOpts = isOuterUrlWithoutCORS?{ mode: 'no-cors' }:undefined;
+                var fetchOpts = /*isOuterUrlWithoutCORS?{ mode: 'no-cors' }:*/undefined;
                 return fetch(request, fetchOpts).then(function assetFetched(response) {
-                    if(response && (response.ok || isOuterUrlWithoutCORS)) {
-                        cache.put(request, response.clone());
-                        console.info("Stored asset into cache : " + request.url);
-                        return response;
+                    if(response /* && (response.ok || fetchOpts) */) {
+                        return storeResponseInCache("asset", cache, request, response);
                     } else {
                         console.info("Bad response from fetch, falling back on cache for : "+request.url);
-                        return cache.match(request);
+                        return findResponseInCache(cache, request);
                     }
                 }, function assetNotFetched() { // Here, good chances we're offline, serving from cache if found
-                    return cache.match(request).then(function (response) {
-                        if(response) {
-                            console.info("Used cached assed for : "+request.url);
-                        } else {
-                            console.error("Asset neither available from network nor from cache : "+request.url);
-                        }
-                        return response;
-                    })
+                    return findResponseInCache(cache, request);
                 })
             })
         );
@@ -48,8 +64,7 @@ this.addEventListener('fetch', function fetcher (event) {
                     } else {
                         return fetch(request, { mode: 'no-cors' }).then(function(response){
                             if(response) {
-                                cache.put(request, response.clone());
-                                console.info("Stored picture into cache : "+request.url);
+                                return storeResponseInCache("picture", cache, request, response);
                             }
                             return response;
                         });
